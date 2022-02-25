393edc50d02d9126f2cac2f27225d2177b4b5459
PavelNiki <p.nikitenko@leviossa.net>
2022-02-25 14:03:52 +0300

    update date

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,72 +1,72 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+// import { PrismaClient } from "@prisma/client";
+// const prisma = new PrismaClient();
 
-async function main() {
-  await prisma.rooms.createMany({
-    data: [{ name: "ruby" }, { name: "noda" }, { name: "storage" }],
-  });
-  await prisma.categories.createMany({
-    data: [{ name: "furniture" }, { name: "device" }],
-  });
-  await prisma.inventory.createMany({
-    data: [
-      {
-        category: "furniture",
-        name: "monitor",
-        roomName: "node",
-        itemImage: "{{$timestamp}}",
-      },
-      {
-        category: "furniture",
-        name: "monitor",
-        roomName: "node",
-        itemImage: "{{$timestamp}}",
-      },
-      {
-        category: "device",
-        name: "PC",
-        roomName: "node",
-        itemImage: "{{$timestamp}}",
-      },
-      {
-        category: "device",
-        name: "PC",
-        roomName: "ruby",
-        itemImage: "{{$timestamp}}",
-      },
-      {
-        category: "device",
-        name: "keybord",
-        roomName: "node",
-        itemImage: "{{$timestamp}}",
-      },
-      {
-        category: "device",
-        name: "mouse",
-        roomName: "node",
-        itemImage: "{{$timestamp}}",
-      },
-      {
-        category: "device",
-        name: "headphones",
-        roomName: "node",
-        itemImage: "{{$timestamp}}",
-      },
-      {
-        category: "device",
-        name: "mouse",
-        roomName: "node",
-        itemImage: "{{$timestamp}}",
-      },
-    ],
-  });
-}
+// async function main() {
+//   await prisma.rooms.createMany({
+//     data: [{ name: "ruby" }, { name: "noda" }, { name: "storage" }],
+//   });
+//   await prisma.categories.createMany({
+//     data: [{ name: "furniture" }, { name: "device" }],
+//   });
+//   await prisma.inventory.createMany({
+//     data: [
+//       {
+//         category: "furniture",
+//         name: "monitor",
+//         roomName: "node",
+//         itemImage: "{{$timestamp}}",
+//       },
+//       {
+//         category: "furniture",
+//         name: "monitor",
+//         roomName: "node",
+//         itemImage: "{{$timestamp}}",
+//       },
+//       {
+//         category: "device",
+//         name: "PC",
+//         roomName: "node",
+//         itemImage: "{{$timestamp}}",
+//       },
+//       {
+//         category: "device",
+//         name: "PC",
+//         roomName: "ruby",
+//         itemImage: "{{$timestamp}}",
+//       },
+//       {
+//         category: "device",
+//         name: "keybord",
+//         roomName: "node",
+//         itemImage: "{{$timestamp}}",
+//       },
+//       {
+//         category: "device",
+//         name: "mouse",
+//         roomName: "node",
+//         itemImage: "{{$timestamp}}",
+//       },
+//       {
+//         category: "device",
+//         name: "headphones",
+//         roomName: "node",
+//         itemImage: "{{$timestamp}}",
+//       },
+//       {
+//         category: "device",
+//         name: "mouse",
+//         roomName: "node",
+//         itemImage: "{{$timestamp}}",
+//       },
+//     ],
+//   });
+// }
 
-main()
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  })
-  .finally(async () => {
-    await prisma.$disconnect();
-  });
+// main()
+//   .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
